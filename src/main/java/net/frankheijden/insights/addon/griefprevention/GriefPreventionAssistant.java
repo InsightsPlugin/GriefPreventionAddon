f3003d5d66cc3be8ee2d71b019c2d157481582cc
Frank van der Heijden <frank.boekanier@gmail.com>
2020-12-27 15:44:10 +0100

    v1.1.0 - Conform to new Insights assistant update

--- src/main/java/net/frankheijden/insights/addon/griefprevention/GriefPreventionAssistant.java
+++ src/main/java/net/frankheijden/insights/addon/griefprevention/GriefPreventionAssistant.java
@@ -2,27 +2,34 @@ package net.frankheijden.insights.addon.griefprevention;
 
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
+import net.frankheijden.insights.entities.Area;
 import net.frankheijden.insights.entities.CacheAssistant;
-import net.frankheijden.insights.entities.Selection;
+import net.frankheijden.insights.entities.CuboidSelection;
 import org.bukkit.Location;
+
+import java.util.Collections;
 
 public class GriefPreventionAssistant extends CacheAssistant {
 
     public GriefPreventionAssistant() {
-        super("GriefPrevention", "claim");
+        super("GriefPrevention", "GriefPrevention", "claim", "v1.1.0");
     }
 
-    public Selection adapt(Claim claim) {
+    public String getId(Claim claim) {
+        return getPluginName() + "@" + claim.getID();
+    }
+
+    public Area adapt(Claim claim) {
         if (claim == null) return null;
         Location min = claim.getLesserBoundaryCorner();
         min.setY(0);
         Location max = claim.getGreaterBoundaryCorner();
         max.setY(max.getWorld().getMaxHeight() - 1);
-        return new Selection(min, max);
+        return Area.from(this, getId(claim), Collections.singletonList(new CuboidSelection(min, max)));
     }
 
     @Override
-    public Selection getSelection(Location location) {
+    public Area getArea(Location location) {
         return adapt(GriefPrevention.instance.dataStore.getClaimAt(location, false, null));
     }
 }
